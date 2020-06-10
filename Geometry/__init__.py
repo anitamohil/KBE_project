5d1bc7d30d9f77bc664a4209e7b1333669977c64
anitamohil <anitamohi@gmail.com>
2020-06-10 17:59:21 +0200

    These are my files that I am working on. To initiate the code, you run full_aircraft.py It still has bugs because of the Xfoil not working correctly but I am trying to fix it and hopefully it is solved by tomorrow. I will keep you updated.

--- Geometry/__init__.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-_module_dir = os.path.dirname(__file__)
-AIRFOIL_DIR = os.path.join(_module_dir, 'airfoil_library', '')
-
-from .airfoil import Airfoil
-from .ref_frame import Frame
-
